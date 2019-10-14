43380dc0dae16bee90a33c309959dd90cc401f7e
JrCampos007 <47427313+JrCampos007@users.noreply.github.com>
2019-10-14 12:52:05 -0700

    Create environment.js

---
+++ environment.js
@@ -0,0 +1,10 @@
+// Week 3: JS Environment
+
+console.log("Lets make sure JavaScript is working.");
+var name = "Jose";
+console.log("Your full name is: " + name + " Campos")
+console.log("The unicode characters of your first name are:")
+for (var i = 0; i < name.length; i++){
+	console.log(name.charCodeAt(i));
+}
+console.log("Copy and paste these values for activity credit.")
